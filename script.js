1c4207d54c4e82c8c8b23eda031525550d0a878a
Tomsooer <tsoer2013@gmail.com>
2025-08-14 16:43:46 +0300

    The finish project

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const menuIcon = document.querySelector('.menu-icon');
 const header = document.querySelector('header');
 const headerNav = document.querySelector('.header__right-content');
 const body = document.querySelector('body');
-console.log('menuIcon: ', menuIcon)
-console.log('header: ', header)
 
 menuIcon.addEventListener('click', () => {
 	menuIcon.classList.toggle('menu-icon--active');
